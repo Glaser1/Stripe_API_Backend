2fcd005c6152f0deae3e3e1ab82f7e99e1b692a4
Aleksandr <sashapliskov@gmail.com>
2025-03-02 20:20:47 +0300

    replace stripe session with sripe paymentintent: - add calculate_order_total function - add get_order_info_for_intent function - add create_payment_intent_for_order - add "secret" view using paymentintent - use currency_converter library to convert all prices currencies to a default currency

--- stripe_payment/items/views.py
+++ stripe_payment/items/views.py
@@ -2,105 +2,77 @@ import stripe
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.csrf import csrf_exempt
+from currency_converter import CurrencyConverter
 
 from items.models import Item, Order
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-def success_page(request):
-    return render(request, template_name="success.html")
+def calculate_order_total(order, target_currency=settings.DEFAULT_CURRENCY):
+    """Считает промежуточную стоимость всех товаров в валюте по умолчанию."""
 
+    total_sum = 0
+    currency_converter = CurrencyConverter()
 
-def cancel_page(request):
-    return render(request, template_name="cancel.html")
-
-
-def create_stripe_product_and_price(item_id):
-    item = get_object_or_404(Item, id=item_id)
-    price = stripe.Price.create(
-        currency=item.currency,
-        product_data={"name": item.name},
-        unit_amount=item.price,
-    )
-    return price
-
-
-def create_stripe_order_list(order):
-    order_list = []
-
-    order_tax = order.tax
-
-    if order_tax:
-        tax_rate = stripe.TaxRate.create(
-            display_name=order_tax.name,
-            percentage=order_tax.percentage,
-            inclusive=False,
-        )
     for item in order.items.all():
-        price = stripe.Price.create(
-            currency=item.currency,
-            product_data={"name": item.name},
-            unit_amount=item.price,
-        )
-        order_list.append({"price": price.id, "quantity": 1, "tax_rates": [tax_rate.id] if order_tax else []})
+        if item.currency != target_currency:
+            converted_price = currency_converter.convert(item.price, item.currency, target_currency)
+            total_sum += converted_price
+        else:
+            total_sum += item.price
+    if order.tax:
+        total_sum += total_sum * order.tax.percentage / 100
 
-    return order_list
-
-
-@require_GET
-def create_checkout_session_for_order(request, order_id):
-    order = get_object_or_404(Order.objects.prefetch_related("items").prefetch_related("discounts"), id=order_id)
-    order_list = create_stripe_order_list(order)
-
-    coupons_ids = []
-
-    if order.discounts.exists():
-        for discount in order.discounts.all():
-            coupon = stripe.Coupon.create(percent_off=discount.percent_off)
-            coupons_ids.append(coupon.id)
-
-    session = stripe.checkout.Session.create(
-        line_items=order_list,
-        mode="payment",
-        success_url=settings.APP_DOMAIN + "/success/",
-        cancel_url=settings.APP_DOMAIN + "/cancel/",
-        discounts=[{"coupon": v} for v in coupons_ids],
-    )
-
-    return JsonResponse({"id": session.id})
-
-
-@require_GET
-def create_checkout_session(request, item_id):
-    price = create_stripe_product_and_price(item_id)
-    session = stripe.checkout.Session.create(
-        line_items=[{"price": price.id, "quantity": 1}],
-        mode="payment",
-        success_url=settings.APP_DOMAIN + "/success/",
-        cancel_url=settings.APP_DOMAIN + "/cancel/",
-    )
-    return JsonResponse({"id": session.id})
-
-
-@require_GET
-def get_item_info(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
-    return render(
-        request, template_name="item.html", context={"item": item, "stripe_public_key": settings.STRIPE_PUBLIC_KEY}
-    )
+    if order.discounts:
+        total_discount = sum(discount.percent_off for discount in order.discounts.all())
+        total_discount = min(total_discount, 100)
+        total_sum -= total_sum * total_discount / 100
+    return int(total_sum / 100)
 
 
 @require_GET
 def get_order_info(request, order_id):
     order = get_object_or_404(Order.objects.prefetch_related("items"), id=order_id)
+    total_sum = calculate_order_total(order)
     return render(
         request,
-        template_name="order.html",
+        template_name="payment_intent.html",
         context={
-            "order_id": order.id,
             "order_items": order.items.all(),
             "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
+            "order_id": order.id,
+            "total_sum": total_sum,
+            "currency": settings.DEFAULT_CURRENCY,
         },
     )
+
+
+def get_order_info_for_intent(order_id):
+    order = get_object_or_404(
+        Order.objects.prefetch_related("discounts").select_related("tax"),
+        pk=order_id,
+    )
+    total_sum = calculate_order_total(order)
+    return {"amount": total_sum, "currency": settings.DEFAULT_CURRENCY}
+
+
+def create_payment_intent_for_order(order_id):
+    """Создает PaymentIntent на основе Order с товарами,"""
+    """ для заказа с товарами в разных валютах в качестве дефолтной выбрана "USD"."""
+
+    order_info = get_order_info_for_intent(order_id)
+    payment_intent = stripe.PaymentIntent.create(
+        amount=order_info["amount"],
+        currency=order_info["currency"],
+    )
+    return payment_intent
+
+
+@csrf_exempt
+@require_GET
+def secret(request, order_id):
+    payment_intent = create_payment_intent_for_order(order_id)
+    return JsonResponse({"client_secret": payment_intent.client_secret})

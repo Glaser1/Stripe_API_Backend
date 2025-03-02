af0cca118b93c39f0886c4381c4602abe6c5acfc
Aleksandr <sashapliskov@gmail.com>
2025-03-02 20:17:42 +0300

    remove unused paths

--- stripe_payment/items/urls.py
+++ stripe_payment/items/urls.py
@@ -4,10 +4,6 @@ from django.urls import path
 app_name = "items"
 
 urlpatterns = [
-    path("item/buy/<int:item_id>/", views.create_checkout_session, name="create-checkout-session"),
-    path("item/<int:item_id>/", views.get_item_info, name="item-info"),
-    path("order/buy/<int:order_id>/", views.create_checkout_session_for_order, name="create-checkout-session-for-order"),
-    path("order/<int:order_id>/", views.get_order_info, name="order-info"),
-    path("success/", views.success_page, name="success"),
-    path("cancel/", views.cancel_page, name="cancel"),
+    path("order/<int:order_id>/", views.get_order_info, name="get_order_info"),
+    path("create-payment-intent/<int:order_id>", views.secret, name="create_payment_intent"),
 ]

299157733a984d8da798b3293feeb7e4c48a33d4
Aleksandr <sashapliskov@gmail.com>
2025-03-02 15:45:34 +0300

    refactore: move DEBUG и SECRET_KEY to environment variables

--- stripe_payment/stripe_payment/settings.py
+++ stripe_payment/stripe_payment/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l9*+y=w8tr+o*&6**p+3r@woae$$&l4opbx_6ggwpd+q7u%dcg"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DJANGO_DEBUG", False))
 
 ALLOWED_HOSTS = ["*"]
 
